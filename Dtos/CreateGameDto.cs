4b7e8541c974e34696959dec971f1691cb7ab224
wstoccob <alinur.seysekov@gmail.com>
2024-05-03 13:00:36 +0500

    Add GameDto record

---
+++ Dtos/CreateGameDto.cs
@@ -0,0 +1,3 @@
+﻿namespace GameStore.Dtos;
+
+public record CreateGameDto();
--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+using GameStore.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new GameDto(
+        1,
+        "Street Fighter II",
+        "Fighting",
+        21.99m,
+        new DateOnly(1992, 7, 15)),
+    new GameDto(
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99m,
+        new DateOnly(2010, 9, 30)),
+    new GameDto(
+        3,
+        "Third game",
+        "SampleGenre",
+        111.89m,
+        new DateOnly(2020, 2, 9))
+];
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
