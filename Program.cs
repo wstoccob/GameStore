10d0c86dd9a5b56612b562533ae6cedff16f847b
wstoccob <alinur.seysekov@gmail.com>
2024-05-05 15:07:52 +0500

    Implement DELETE request

--- Program.cs
+++ Program.cs
@@ -61,4 +61,12 @@ app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
     return Results.NoContent();
 });
 
+// DELETE /games/1
+app.MapDelete("games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
 app.Run();
