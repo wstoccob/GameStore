fd7ad3d56723aebfdc1a802f3bbc494e3d39268b
wstoccob <alinur.seysekov@gmail.com>
2024-05-05 15:50:28 +0500

    Fix error with getting invalid response when trying to change a non-existing element with PUT request

--- Program.cs
+++ Program.cs
@@ -57,6 +57,12 @@ app.MapPost("games", (CreateGameDto newGame) =>
 app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
+    
     games[index] = new GameDto(
         id,
         updatedGame.Name,
